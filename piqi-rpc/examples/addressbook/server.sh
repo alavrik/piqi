4d84d0a288e77fd4f43c1f14b131bda12a501469
Anton Lavrik <alavrik@piqi.org>
2011-03-25 03:01:30 -0500

    (piqi-rpc) Added server and client test scenarios for Piqi-RPC example.

---
+++ piqi-rpc/examples/addressbook/server.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+PA="-pa $PIQI_ROOT/piqi-erlang/ebin -pa $PIQI_ROOT/piqi-rpc/ebin"
+
+RUN='piqi_rpc:add_service({addressbook, addressbook_piqi_rpc, "addressbook"}).'
+
+erl $PA -noshell -s piqi_rpc start -eval "$RUN"
+
