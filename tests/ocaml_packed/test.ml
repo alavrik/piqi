b93183309f16ef7e7ee12436faf1026dbbb54915
Anton Lavrik <alavrik@piqi.org>
2011-05-27 01:17:51 -0500

    Add Piqi tools, OCaml and Erlang tests for packed encoding
    There are also some tests for .ocaml-array

---
+++ tests/ocaml_packed/test.ml
@@ -0,0 +1,16 @@
+
+let t () =
+  let ich = open_in "test-all.piq.pb" in
+  let buf = Piqirun.init_from_channel ich in
+  let piqi = Packed_piqi.parse_r_all buf in
+
+  let och = open_out "test-all.piq.pb.pb" in
+  let data = Packed_piqi.gen_r_all piqi in
+  Piqirun.to_channel och data;
+
+  close_in ich;
+  close_out och;
+  ()
+
+
+let _ = t ()
---
+++ tests/ocaml_packed/test_packed_array.ml
@@ -0,0 +1,16 @@
+
+let t () =
+  let ich = open_in "test-all.piq.pb" in
+  let buf = Piqirun.init_from_channel ich in
+  let piqi = Packed_array_piqi.parse_r_all buf in
+
+  let och = open_out "test-all.piq.pb.array" in
+  let data = Packed_array_piqi.gen_r_all piqi in
+  Piqirun.to_channel och data;
+
+  close_in ich;
+  close_out och;
+  ()
+
+
+let _ = t ()
