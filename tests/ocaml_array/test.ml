b993877b722cb2ae2f77465c74251026566db231
Anton Lavrik <alavrik@piqi.org>
2011-05-30 23:35:21 -0500

    (tests) Imrove tests for packed fields and lists
    Also, add tests for OCaml array representation for unpacked Piqi repeated fields
    and lists.

--- tests/ocaml_array/test.ml
+++ tests/ocaml_array/test.ml
@@ -1,10 +1,11 @@
 
 let t () =
+  print_endline "testing Piqi repeated fields and Piqi lists represented as OCaml arrays";
   let ich = open_in "test-all.piq.pb" in
   let buf = Piqirun.init_from_channel ich in
   let piqi = Packed_piqi.parse_r_all buf in
 
-  let och = open_out "test-all.piq.pb.pb" in
+  let och = open_out "test-all.piq.pb.array" in
   let data = Packed_piqi.gen_r_all piqi in
   Piqirun.to_channel och data;
 
--- tests/ocaml_packed/test_packed_array.ml
+++ tests/ocaml_packed/test_packed_array.ml
@@ -1,10 +1,11 @@
 
 let t () =
+  print_endline "testing packed repeated fields represented as OCaml arrays";
   let ich = open_in "test-all.piq.pb" in
   let buf = Piqirun.init_from_channel ich in
   let piqi = Packed_array_piqi.parse_r_all buf in
 
-  let och = open_out "test-all.piq.pb.array" in
+  let och = open_out "test-all.piq.pb.packed-array" in
   let data = Packed_array_piqi.gen_r_all piqi in
   Piqirun.to_channel och data;
 
