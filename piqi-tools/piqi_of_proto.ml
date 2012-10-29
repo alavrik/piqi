3c8f7005bb99e84584c8b50eddd564e828a299ce
Anton Lavrik <alavrik@piqi.org>
2012-10-29 02:40:52 -0500

    Add ./configure script and autodetect dependencies
    Don't build ulex, xmlm and easy-format third-party dependencies if they are
    already installed in the system.
    
    Even if they are not installed and we need to build them from deps/ don't
    include them in the piqi OCaml package. Instead, install them as separate
    packages as a part of "make ocaml-install" step.
    
    These changes will allows us to create Piqi distribution packages more easily.

--- piqi-tools/piqi_of_proto.ml
+++ piqi-tools/piqi_of_proto.ml
@@ -1,4 +1,4 @@
-(*pp camlp4o -I `ocamlfind query piqi.ulex` -I `ocamlfind query piqi.syntax` pa_labelscope.cmo pa_openin.cmo pa_ulex.cma *)
+(*pp camlp4o -I `ocamlfind query ulex` -I `ocamlfind query piqi.syntax` pa_labelscope.cmo pa_openin.cmo pa_ulex.cma *)
 (*
    Copyright 2009, 2010, 2011, 2012 Anton Lavrik
 
