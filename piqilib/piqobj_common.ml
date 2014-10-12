687a238c152c2bd6c788f077b59e2305e8a55037
Anton Lavrik <alavrik@piqi.org>
2014-10-12 16:24:31 -0700

    Bump copyright year

--- piqilib/piqobj_common.ml
+++ piqilib/piqobj_common.ml
@@ -1,5 +1,5 @@
 (*
-   Copyright 2009, 2010, 2011, 2012, 2013 Anton Lavrik
+   Copyright 2009, 2010, 2011, 2012, 2013, 2014 Anton Lavrik
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
