804868d738ee7594b86416a32e4f0bb3c6a22281
Anton Lavrik <alavrik@piqi.org>
2016-05-31 01:39:11 -0700

    More travis-ci tweaks

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -17,12 +17,34 @@ then
 else
     # install ocaml, install basic build dependencies, run standard build
 
-    if [ "$OCAML_VERSION" = "system" ]
+    if [ "$TRAVIS_OS_NAME" = "osx" ]
     then
-        # ignore this branch on Mac Os X
-        test "$TRAVIS_OS_NAME" = "osx" && exit 0
+        brew update
 
-        # build dependencies
+        # build dependencies (Mac OS X)
+        brew install ocaml opam
+
+        # optional dependencies for running tests
+        brew install protobuf
+
+        echo OCaml version
+        ocaml -version
+
+        echo OPAM versions
+        opam --version
+        opam --git-version
+
+        export OPAMYES=1
+
+        opam init
+        eval `opam config env`
+
+        # install basic build dependencies using opam
+        opam install ocamlfind camlp4
+
+    elif [ "$OCAML_VERSION" = "system" ]
+    then
+        # build dependencies (Ubuntu)
         sudo apt-get install ocaml-nox camlp4-extra ocaml-findlib
 
         # optional dependencies for running tests and building docs
@@ -33,7 +55,6 @@ else
 
         echo "system OCaml version:"
         ocaml -version
-
     else
         # install specific ocaml version
         wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-ocaml.sh
